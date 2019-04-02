f493716889123ba8a2e1e4bd4af5d8f8e531287f
wm-dima <somename15q@gmail.com>
2019-04-02 23:43:44 +0300

    added wo templates

--- wp-content/themes/gospodar/index.php
+++ wp-content/themes/gospodar/index.php
@@ -4,53 +4,7 @@ Template Name: index
 */
 ?>
 <?php get_header(); ?>
-		<div class="main-slider">
-			<div class="swiper-container">
-				<div class="swiper-wrapper">
-					<div class="swiper-slide">
-						<div class="swiper-pic"></div>
-						<div class="swiper-content">
-							<div class="swiper-content__inside">
-								<h2>Посуда</h2>
-								<p>для Вашей кухни <br> по оптовым ценам</p>
-							</div>	
-						</div>
-					</div>
-					<div class="swiper-slide">
-						<div class="swiper-pic"></div>
-						<div class="swiper-content">
-							<div class="swiper-content__inside">
-								<h2>Посуда</h2>
-								<p>для Вашей кухни <br> по оптовым ценам</p>
-							</div>	
-						</div>
-					</div>
-					<div class="swiper-slide">
-						<div class="swiper-pic"></div>
-						<div class="swiper-content">
-							<div class="swiper-content__inside">
-								<h2>Посуда</h2>
-								<p>для Вашей кухни <br> по оптовым ценам</p>
-							</div>	
-						</div>
-					</div>
-					<div class="swiper-slide">
-						<div class="swiper-pic"></div>
-						<div class="swiper-content">
-							<div class="swiper-content__inside">
-								<h2>Посуда</h2>
-								<p>для Вашей кухни <br> по оптовым ценам</p>
-							</div>	
-						</div>
-					</div>
-				</div>
-				<!-- Add Pagination -->
-				<div class="swiper-pagination"></div>
-				<!-- Add Arrows -->
-				<div class="swiper-button-next"></div>
-				<div class="swiper-button-prev"></div>
-			</div>
-		</div>
+
 
 		  <div class="free-delivery">
 			  <div class="wrapper">
---
+++ wp-content/themes/gospodar/template-parts/main-slider.php
@@ -0,0 +1,47 @@
+<div class="main-slider">
+	<div class="swiper-container">
+		<div class="swiper-wrapper">
+			<div class="swiper-slide">
+				<div class="swiper-pic"></div>
+				<div class="swiper-content">
+					<div class="swiper-content__inside">
+						<h2>Посуда</h2>
+						<p>для Вашей кухни <br> по оптовым ценам</p>
+					</div>	
+				</div>
+			</div>
+			<div class="swiper-slide">
+				<div class="swiper-pic"></div>
+				<div class="swiper-content">
+					<div class="swiper-content__inside">
+						<h2>Посуда</h2>
+						<p>для Вашей кухни <br> по оптовым ценам</p>
+					</div>	
+				</div>
+			</div>
+			<div class="swiper-slide">
+				<div class="swiper-pic"></div>
+				<div class="swiper-content">
+					<div class="swiper-content__inside">
+						<h2>Посуда</h2>
+						<p>для Вашей кухни <br> по оптовым ценам</p>
+					</div>	
+				</div>
+			</div>
+			<div class="swiper-slide">
+				<div class="swiper-pic"></div>
+				<div class="swiper-content">
+					<div class="swiper-content__inside">
+						<h2>Посуда</h2>
+						<p>для Вашей кухни <br> по оптовым ценам</p>
+					</div>	
+				</div>
+			</div>
+		</div>
+		<!-- Add Pagination -->
+		<div class="swiper-pagination"></div>
+		<!-- Add Arrows -->
+		<div class="swiper-button-next"></div>
+		<div class="swiper-button-prev"></div>
+	</div>
+</div>
