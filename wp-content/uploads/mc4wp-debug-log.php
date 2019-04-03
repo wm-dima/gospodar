261b72c97dd7c9701a111c89ef4932aad97766e2
wm-dima <somename15q@gmail.com>
2019-04-03 20:10:42 +0300

    install mailchimp

---
+++ wp-content/uploads/mc4wp-debug-log.php
@@ -0,0 +1,3 @@
+<?php exit; ?>
+[2019-04-03 20:08:42] WARNING: Form 216 > some*******@gm***.com is already subscribed to the selected list(s)
+[2019-04-03 20:08:51] WARNING: Form 216 > some*******@gm***.com is already subscribed to the selected list(s)
