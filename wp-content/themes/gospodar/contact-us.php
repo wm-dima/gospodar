0be17c8d81d090cae65aead1c79e6fbd2ec98b42
wm-dima <somename15q@gmail.com>
2019-04-10 12:02:57 +0300

    server pull

---
+++ wp-content/themes/gospodar/contact-us.php
@@ -0,0 +1,8 @@
+<?php
+/*
+Template Name: contact to us
+*/
+?>
+<?php get_header(); ?>
+<?php the_content(); ?>
+<?php get_footer(); ?>
---
+++ wp-content/themes/gospodar/security-policy.php
@@ -0,0 +1,8 @@
+<?php
+/*
+Template Name: security info
+*/
+?>
+<?php get_header(); ?>
+<?php the_content(); ?>
+<?php get_footer(); ?>
